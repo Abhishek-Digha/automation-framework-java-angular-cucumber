15c7af6ecc964ad9e7d442b4d3c95d265ccb2925
simonk <jackofspaces@gmail.com>
2018-01-05 18:53:10 +0000

    javadocs tweaks

--- src/test/java/com/simonkay/javaframework/stepdefinitions/GeneralSteps.java
+++ src/test/java/com/simonkay/javaframework/stepdefinitions/GeneralSteps.java
@@ -15,9 +15,7 @@ public class GeneralSteps extends AbstractBaseStepDefinition {
 		super.before(scenario);
 	}
 	
-	/*
-	 * Creates a new order and adds it to the cucumberWorld object for shared state among stepdefinitions
-	 */
+
 	@Given("^The order \"([^\"]*)\" exists$")
 	public void the_order_exists(String orderAlias) throws Throwable {
 		Order order = new Order("Credit card", new DataProviderInjector());
