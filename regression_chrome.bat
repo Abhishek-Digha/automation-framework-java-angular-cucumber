d598da18e303dbec57e496517cedfba52004852d
simonk <jackofspaces@gmail.com>
2018-01-07 13:29:22 +0000

    fixes #3

--- regression_chrome.bat
+++ regression_chrome.bat
@@ -1,4 +1,3 @@
 call mvn clean
 call mvn test -Dbrowser=chrome -Drun.on.browserstack.grid.local=local -Dcucumber.Options="--plugin io.qameta.allure.cucumberjvm.AllureCucumberJvm --tags @Regression"
-call mvn allure:generate
 
