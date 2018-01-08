e26021496e011770899289dda7981e435593f7fd
simonk <jackofspaces@gmail.com>
2018-01-08 11:34:21 +0000

    adding grid support, modifying regression_chrome.bat to not overwrite local/grid

--- regression_chrome.bat
+++ regression_chrome.bat
@@ -1,3 +1,3 @@
 call mvn clean
-call mvn test -Dbrowser=chrome -Druntags=@Regression -Drun.on.browserstack.grid.local=local -Dcucumber.Options="--plugin io.qameta.allure.cucumberjvm.AllureCucumberJvm"
+call mvn test -Dbrowser=chrome -Druntags=@Regression -Dcucumber.Options="--plugin io.qameta.allure.cucumberjvm.AllureCucumberJvm"
 
