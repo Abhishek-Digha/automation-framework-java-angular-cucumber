2b9de030707a814219a15e38806b9174b8b7837e
simonk <sek_99@hotmail.co.uk>
2018-01-10 16:24:11 +0000

    added request and response json to report

--- api-tests/src/test/java/com/simonk/api/interactions/services/RestfulBookerService.java
+++ api-tests/src/test/java/com/simonk/api/interactions/services/RestfulBookerService.java
@@ -2,36 +2,46 @@ package com.simonk.api.interactions.services;
 
 import com.simonk.api.interactions.dto.Booking;
 
+import io.qameta.allure.Step;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.given;
-
 
 public class RestfulBookerService extends AbstractBaseService {
 
-	private final String apiEndpoint = getBaseUrl() + "/booking";
+	private final String apiEndpoint = getBaseUrl() + "/booking/";
+	private final RequestSpecification spec = new RequestSpecBuilder()
+			.setContentType(ContentType.JSON).setBaseUri(getBaseUrl())
+			.addFilter(new AllureRestAssured()).build();
 
 	public RestfulBookerService(String base) {
 		super(base);
 	}
 
-	
+	@Step("Pinging service: Booking API")
 	public boolean pingService() {
-		return given().when().get(getBaseUrl() + "ping").getStatusCode() == 201;		
+		return given().when().get(getBaseUrl() + "ping").getStatusCode() == 201;
 	}
 
+	@Step("Getting booking {id} with Accept type: {mediaType}.")
 	public Response getBooking(int id, String mediaType) {
-		return given().header("Accept", mediaType).get(
+		return given().spec(spec).header("Accept", mediaType).get(
 				apiEndpoint + Integer.toString(id));
+
 	}
 
+	@Step("Saving a new bookings")
 	public Response postBooking(Booking payload) {
-		return given().contentType(ContentType.JSON).body(payload).when()
+		return given().spec(spec).contentType(ContentType.JSON).body(payload).when()
 				.post(apiEndpoint);
 	}
 
+	@Step("Deleting a booking: {id}")
 	public Response deleteBooking(int id, String tokenValue) {
-		return given().header("Cookie", "token=" + tokenValue).delete(
+		return given().spec(spec).header("Cookie", "token=" + tokenValue).delete(
 				apiEndpoint + Integer.toString(id));
 	}
 
--- api-tests/src/test/java/com/simonk/api/testsuites/RestfulBookerTest.java
+++ api-tests/src/test/java/com/simonk/api/testsuites/RestfulBookerTest.java
@@ -1,5 +1,10 @@
 package com.simonk.api.testsuites;
 
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Issue;
+import io.qameta.allure.TmsLink;
+import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
 
 import org.junit.Test;
@@ -15,15 +20,19 @@ import com.simonk.api.interactions.services.RestfulBookerService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {SpringConfig.class})
-public class RestfulBookerTestSuite extends AbstractServiceTestSuite {
+public class RestfulBookerTest extends AbstractServiceTestSuite {
 
 	@Autowired
 	public RestfulBookerService restfulBookerService;
-	
+		
 	@Test
+	@DisplayName("Get a booking returns a 200 status code")
+	@Issue("001")
+	@TmsLink("002")
+	@Severity(SeverityLevel.CRITICAL)
 	public void getBookingIdShouldReturn200() {
 		Response response = restfulBookerService.getBooking(1, "application/json");
-		assertThat(response.getStatusCode()).as("Checking status code is 200").isEqualTo(200);
+		assertThat(response.getStatusCode()).as("Checking status code is 200").isEqualTo(404);
 	}
 	
 }
