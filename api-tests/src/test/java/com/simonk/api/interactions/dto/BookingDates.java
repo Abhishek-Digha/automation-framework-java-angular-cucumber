4d3e0b7b16e4ba999b6eb8664657260d86dd81ae
simonk <sek_99@hotmail.co.uk>
2018-01-10 17:33:42 +0000

    api tests fixed, added dto

---
+++ api-tests/src/test/java/com/simonk/api/interactions/dto/BookingDates.java
@@ -0,0 +1,51 @@
+package com.simonk.api.interactions.dto;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BookingDates {
+	@JsonProperty
+	private Date checkin;
+	@JsonProperty
+	private Date checkout;
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+	public Date getCheckout() {
+		return checkout;
+	}
+
+	// default constructor required by Jackson
+	private BookingDates() {
+		// nothing here
+	}
+
+	private BookingDates(Date checkin, Date checkout) {
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+
+	public static class Builder {
+
+		private Date checkin;
+		private Date checkout;
+
+		public Builder setCheckin(Date checkin) {
+			this.checkin = checkin;
+			return this;
+		}
+
+		public Builder setCheckout(Date checkout) {
+			this.checkout = checkout;
+			return this;
+		}
+
+		public BookingDates build() {
+			return new BookingDates(checkin, checkout);
+		}
+	}
+
+}
--- api-tests/src/test/java/com/simonk/api/testsuites/RestfulBookerTest.java
+++ api-tests/src/test/java/com/simonk/api/testsuites/RestfulBookerTest.java
@@ -1,5 +1,7 @@
 package com.simonk.api.testsuites;
 
+import java.util.Date;
+
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Issue;
@@ -17,25 +19,28 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static org.assertj.core.api.Assertions.*;
 
 import com.simonk.api.config.spring.SpringConfig;
+import com.simonk.api.interactions.dto.Booking;
+import com.simonk.api.interactions.dto.BookingDates;
 import com.simonk.api.interactions.services.RestfulBookerService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {SpringConfig.class})
+@ContextConfiguration(classes = { SpringConfig.class })
 public class RestfulBookerTest extends AbstractServiceTestSuite {
 
 	@Autowired
 	public RestfulBookerService restfulBookerService;
-		
+
 	@Test
-	@DisplayName("Get booking by id returns 200 ok")
+	@DisplayName("Testing retrieving a valid booking")
 	@Issue("001")
 	@TmsLink("001")
 	@Severity(SeverityLevel.CRITICAL)
 	public void getBookingIdShouldReturn200() {
 		Response response = restfulBookerService.getBooking(1);
-		assertThat(response.getStatusCode()).as("Checking status code is 200").isEqualTo(200);
+		assertThat(response.getStatusCode()).as("Checking status code is 200")
+				.isEqualTo(200);
 	}
-	
+
 	@Test
 	@DisplayName("Get a booking returns a 200 ok")
 	@Issue("001")
@@ -43,30 +48,51 @@ public class RestfulBookerTest extends AbstractServiceTestSuite {
 	@Severity(SeverityLevel.CRITICAL)
 	public void getBookingsShouldReturn200() {
 		Response response = restfulBookerService.getBookings();
-		assertThat(response.getStatusCode()).as("Get all bookings returns 200").isEqualTo(200);
+		assertThat(response.getStatusCode()).as("Get all bookings returns 200")
+				.isEqualTo(200);
 	}
-	
+
 	@Test
-	@DisplayName("Pinging the service returns 201 Created")
+	@DisplayName("Testing pinging the service")
 	@Issue("003")
 	@TmsLink("003")
 	@Severity(SeverityLevel.CRITICAL)
 	public void pingBookingsShouldReturn201() {
 		Response response = restfulBookerService.pingService();
 		SoftAssertions softly = new SoftAssertions();
-		softly.assertThat(response.getStatusCode()).as("Ping should return 201").isEqualTo(201);
-		softly.assertThat(response.getStatusLine()).as("Ping returns Created").contains("Created");
+		softly.assertThat(response.getStatusCode())
+				.as("Ping should return 201").isEqualTo(201);
+		softly.assertThat(response.getStatusLine()).as("Ping returns Created")
+				.contains("Created");
 		softly.assertAll();
 	}
-	
+
 	@Test
-	@DisplayName("Pinging the service returns 201 Created")
+	@DisplayName("Testing retrieving non existing booking")
 	@Issue("004")
 	@TmsLink("004")
 	@Severity(SeverityLevel.CRITICAL)
 	public void getBookingWithNonExistingIdShouldReturn404() {
 		Response response = restfulBookerService.getBooking(252525);
-		assertThat(response.getStatusCode()).as("Non existing booking id returns 404").isEqualTo(404);
+		assertThat(response.getStatusCode()).as(
+				"Non existing booking id returns 404").isEqualTo(404);
 	}
-	
+
+	@Test
+	@DisplayName("Testing adding a new booking")
+	@Issue("004")
+	@TmsLink("004")
+	@Severity(SeverityLevel.CRITICAL)
+	public void postBookingReturns200() {
+		BookingDates dates = new BookingDates.Builder().setCheckin(new Date()).setCheckout(new Date()).build();
+		Booking booking = new Booking.Builder().setFirstname("Simon").setLastname("K")
+				.setTotalprice(200)
+                .setDepositpaid(true)
+                .setBookingdates(dates)
+                .setAdditionalneeds("None")
+                .build();
+
+		Response response = restfulBookerService.postBooking(booking);
+		assertThat(response.getStatusCode()).as("Posting booking returns 200").isEqualTo(200);
+	}
 }
